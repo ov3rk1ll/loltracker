f7e8032df1b610fc0b54fdd6ca3a6c4e62e520c9
ov3rk1ll <overkillerror@gmail.com>
2021-10-13 17:10:24 +0200

    test game loading

--- tests/unit/game.spec.ts
+++ tests/unit/game.spec.ts
@@ -1,33 +1,33 @@
 import { shallowMount } from "@vue/test-utils";
-import mockAxios from "jest-mock-axios";
-
-import { summoner } from "@/data/dummy-data";
+import axios from "axios";
 
 import Game from "@/components/Game.vue";
 
-jest.mock("axios", () => mockAxios);
-
-afterEach(() => {
-  // cleaning up the mess left behind the previous test
-  mockAxios.reset();
-});
+jest.unmock("axios");
 
 describe("Game.vue", () => {
   it("requests API when mounted", async () => {
+    const spyCreate = jest.spyOn(axios, "create");
+
     shallowMount(Game, {
       propsData: { summonerName: "test", region: "euw1" },
     });
 
-    expect(mockAxios.create).toHaveBeenCalledWith({
+    expect(spyCreate).toHaveBeenCalledWith({
       baseURL: `https://lol-tracker-spectator.herokuapp.com/euw1`,
     });
+  });
 
-    expect(mockAxios.get).toHaveBeenCalledWith(
-      "/summoner/v4/summoners/by-name/test"
-    );
+  /* eslint-disable @typescript-eslint/no-explicit-any */
+  it("more", async () => {
+    const wrapper = shallowMount(Game, {
+      propsData: { summonerName: "test", region: "euw1", demoMode: true },
+    });
 
-    mockAxios.mockResponse({ data: { data: summoner } });
+    expect((wrapper.vm as any).loading).toEqual(true);
 
-    await Promise.resolve();
+    await (wrapper.vm as any).api.loadStaticData();
+
+    expect((wrapper.vm as any).loading).toEqual(false);
   });
 });
